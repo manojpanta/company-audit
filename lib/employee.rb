f8c4758c87a586505bd97d2be83b9be1a6bad344
Manoj Panta <manojpanta95@gmail.com>
2018-02-27 09:53:35 -0700

    attributes initialized

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,8 +1,17 @@
 class Employee
 
-  def initialize
+  attr_reader :name, :role, :start_date, :end_date
 
-    
+  def initialize(employee_id, name, role, start_date, end_date)
+    @employee_id = employee_id
+    @name = name
+    @role = role
+    @start_date = start_date
+    @end_date = end_date
+  end
+
+  def employee_id
+    @employee_id.to_i
   end
 
 
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,12 +3,23 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
-    employee = Employee.new
+    employee = Employee.new('5', 'Sally Jacskon', 'Engineer', '2015-01-01', '2018-01-01')
+
     assert_instance_of Employee, employee
   end
 
-  
+
 
   def test_attributes
+    employee = Employee.new('5', 'Sally Jackson', 'Engineer', '2015-01-01', '2018-01-01')
+    assert_equal 'Sally Jackson', employee.name
+    assert_equal 5, employee.employee_id
+    assert_equal 'Engineer', employee.role
+    assert_equal '2015-01-01', employee.start_date
+
+
+
+
+
   end
 end
